564fcdd3b45eceefc6a33182d0d2673135de5d08
Goksu Susuzlu <60940192+GoksuSusuzlu@users.noreply.github.com>
2022-02-09 23:02:28 +0300

    First commit

---
+++ com/example/app/ws/ui/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.app.ws.ui.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.ws.exceptions.UserServiceException;
+import com.example.app.ws.service.UserService;
+import com.example.app.ws.shared.dto.UserDto;
+import com.example.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.example.app.ws.ui.model.response.ErrorMessages;
+import com.example.app.ws.ui.model.response.UserRest;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping(path = "/{id}")
+	public UserRest getUser(@PathVariable String id) {
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = userService.getUserByUserId(id);
+		BeanUtils.copyProperties(userDto, returnValue);
+		
+		return returnValue;
+	}
+	
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
+		UserRest returnValue = new UserRest();
+		
+		if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		return returnValue;
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+	}
+}
